98b851a9c2be2b7c5209a07d4d5df0cff2e2df1d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-07 11:16:31 -0300

    feat(Header): componente Header criado

--- dev-mindset/src/App.jsx
+++ dev-mindset/src/App.jsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header";
 
 
 function App() {
   return (
     <>
-      <h1>Projeto Dev Mindset</h1>
+      <Header/>
     </>
   )
 }
---
+++ dev-mindset/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Dev Mindset</h1>
+
+    </div>
+  )
+}
+
+export default Header
