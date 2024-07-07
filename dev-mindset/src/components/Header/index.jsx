1948fd9f9e8c39e3de76577e1e54f4853656b454
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-07 11:43:50 -0300

    feat(Header): titulo criado elinks

--- dev-mindset/src/components/Header/index.jsx
+++ dev-mindset/src/components/Header/index.jsx
@@ -1,11 +1,24 @@
-
 const Header = () => {
   return (
     <div>
-      <h1>Dev Mindset</h1>
-
+      <header>
+        <nav id="navbar">
+          <div className="nav-brand">
+            <img src="" alt="" />
+            <h1>Dev Mindset</h1>
+          </div>
+          <div>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Trending</a></li>
+                <li><a href="">contact</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
