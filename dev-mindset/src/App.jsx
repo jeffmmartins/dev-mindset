6394c2f938a0e37e4072c775d2c16514f6b61eda
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-07 19:01:28 -0300

    feat(Article): adicionando imagem no componente article

--- dev-mindset/src/App.jsx
+++ dev-mindset/src/App.jsx
@@ -1,11 +1,13 @@
 import "./style/App.css"
 import {Header} from "./components/Header";
+import { Article } from "./components/Article";
 
 
 function App() {
   return (
     <>
       <Header/>
+      <Article/>
     </>
   )
 }
--- dev-mindset/src/components/Article/index.jsx
+++ dev-mindset/src/components/Article/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import imgData from '../../assets/img/logo.png'
 
 export const Article = () => {
   return (
     <div>
-      
+      <article id='article'>
+        <div>
+            <img src={imgData} alt="foto noticias" style={{width: "100px", height: "50px"}}/>
+        </div>
+        <div id='articleData'>
+            <h2>Subtitulo</h2>
+            <h3>Texto</h3>
+            <p>Conteudo</p>
+        </div>
+      </article>
     </div>
   )
 }
