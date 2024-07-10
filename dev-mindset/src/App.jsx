a7e66cd62d305db5c1dc215740ad75f2c1a88d4e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-10 17:45:07 -0300

    feat: criando função async

--- dev-mindset/src/App.jsx
+++ dev-mindset/src/App.jsx
@@ -2,13 +2,18 @@ import "./style/App.css"
 import {Header} from "./components/Header";
 import { Article } from "./components/Article";
 import { useEffect, useState } from "react";
-
+import axios from "axios"
 
 function App() {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
-    
+    async function loadnews(){
+      const response = await axios.get('https://api.spaceflightnewsapi.net/v4/articles')
+      const newsData = response.data.results;
+      console.log(newsData)
+    }
+    loadnews()
   },[])
 
   return (
