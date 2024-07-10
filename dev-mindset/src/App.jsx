863af321f3faf70b5415f09deefde9a5b6331102
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-10 17:12:55 -0300

    feat: instalando o yarn

--- dev-mindset/src/App.jsx
+++ dev-mindset/src/App.jsx
@@ -1,16 +1,23 @@
 import "./style/App.css"
 import {Header} from "./components/Header";
 import { Article } from "./components/Article";
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    
+  },[])
+
   return (
     <>
       <Header/>
       <section id="article">
         <Article title= "Dicas de JS"/>
         <Article title= "Dicas React"/>
-        <Article/>
+        <Article title = "Dicas"/>
       </section> 
     </>
   )
